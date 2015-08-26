9633467c35ca9edb4798dfbde6641a24d2922329
shengyu <shengyu7697@gmail.com>
2015-08-26 15:09:19 +0800

    Accept multiple clients

--- server.c
+++ server.c
@@ -9,12 +9,14 @@
 
 int main(int argc, char* argv[])
 {
-    int sock_fd, new_fd;
+    int sock_fd, new_fd, max_fd;
     socklen_t addrlen;
     struct sockaddr_in my_name, peer_name;
     int status;
-    char buf[MAX_BUF];
+    char recv_buf[MAX_BUF], send_buf[MAX_BUF];
     int nbytes;
+    int i, ret;
+    fd_set read_fds_master, read_fds;
 
     if (argc < 2) {
         fprintf(stderr, "Usage: %s port_number\n", argv[0]);
@@ -46,30 +48,63 @@ int main(int argc, char* argv[])
     }
 
     addrlen = sizeof(peer_name);
+    // set up variables for select
+    max_fd = sock_fd;
+    FD_ZERO(&read_fds_master);
+    FD_SET(sock_fd, &read_fds_master);
 
-    while(1) {
-        // wait for a connection
-        new_fd = accept(sock_fd, (struct sockaddr *)&peer_name, &addrlen);
-        printf("accecpt fd%d\n", new_fd);
-
-        if (new_fd == -1) {
-            perror("Wrong connection");
-            exit(1);
+    while(1)
+    {
+        // copy fd set
+        read_fds = read_fds_master;
+        ret = select(max_fd+1, &read_fds, NULL, NULL, NULL);
+        if (ret == -1) {
+            perror("Select");
+            return -1;
+        } else if (ret == 0) {
+            printf("Select timeout\n");
+            continue;
         }
 
-        while(1) {
-            memset(buf, 0, sizeof(buf));
-            nbytes = read(new_fd, buf, sizeof(buf));
-            if (nbytes == 0)
-                break;
-            else
-                printf("[read] %s\n", buf);
+        for (i = 0; i < max_fd+1; i++) {
+            if (FD_ISSET(i, &read_fds)) {
+                if (i == sock_fd) {
+                    // wait for a connection
+                    new_fd = accept(sock_fd, (struct sockaddr *)&peer_name, &addrlen);
+                    printf("accecpt fd%d\n", new_fd);
 
-            sprintf(buf, "fd%d", new_fd);
-            write(new_fd, buf, strlen(buf));
-            printf("[write] %s\n", buf);
+                    if (new_fd == -1) {
+                        perror("Wrong connection");
+                        exit(1);
+                    }
+
+                    // add to fd set
+                    FD_SET(new_fd, &read_fds_master);
+                    if (new_fd > max_fd) {
+                        max_fd = new_fd;
+                    }
+                } else {
+                    // already connected socket
+                    memset(recv_buf, 0, sizeof(recv_buf));
+                    nbytes = read(i, recv_buf, sizeof(recv_buf));
+                    if (nbytes == 0) {
+                        // connection closed
+                        close(i);
+                        printf("client fd%d disconnect\n", i);
+
+                        // remove from fd set
+                        FD_CLR(i, &read_fds_master);
+                        break;
+                    } else {
+                        printf("[read from fd%d] %s\n", i, recv_buf);
+                    }
+
+                    sprintf(send_buf, "[fd%d] %s", i, recv_buf);
+                    write(i, send_buf, strlen(send_buf));
+                    printf("[write to fd%d] %s\n", i, send_buf);
+                }
+            }
         }
-        close(new_fd);
     }
 
     return 0;
